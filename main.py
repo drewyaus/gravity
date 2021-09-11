f3b4824a437a0b80a7df5b9920511775a36cdce0
Andrew Bono <andrew.bono@kindredgroup.com>
2021-09-11 15:38:28 +0200

    Basic animation

--- main.py
+++ main.py
@@ -2,10 +2,29 @@
 
 # Press Skift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
 
 import pygame
 
-SCREENRECT = pygame.Rect(0, 0, 335 * 3, 186 * 3)
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+CLOCK = pygame.time.Clock()
+SCREEN = pygame.display.set_mode((500, 400), 0, 32)
+
+
+class Dot:
+
+    def __init__(self, x, y):
+        self.velocity = pygame.math.Vector2(1, 1)
+        self.rect = pygame.Rect(x, y, 10, 10)
+        pygame.draw.rect(SCREEN, WHITE, self.rect)
+
+    def update(self):
+        old_rect = pygame.Rect(self.rect)
+        self.rect.topleft = pygame.math.Vector2(self.rect.topleft) + self.velocity
+        print(f'Updating... topleft = {self.rect.topleft}')
+        pygame.draw.rect(SCREEN, BLACK, old_rect)
+        pygame.draw.rect(SCREEN, WHITE, self.rect)
 
 
 def print_hi(name):
@@ -13,14 +32,23 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def main(winstyle=0):
+def main():
     if pygame.get_sdl_version()[0] == 2:
         pygame.mixer.pre_init(44100, 32, 2, 1024)
     pygame.init()
 
-    winstyle = 0
-    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
-    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+    dot = Dot(0, 200)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        dot.update()
+
+        pygame.display.update()
+        CLOCK.tick(60)
 
 
 # Press the green button in the gutter to run the script.
