7ae6c36017f7a299998143fdbe83634766e6ee2f
samyaus <sam.ad.bono@gmail.com>
2021-09-11 12:48:37 +0200

    Started work

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 import pygame
 
 SCREENRECT = pygame.Rect(0, 0, 335 * 3, 186 * 3)
+WHITE = pygame.Color(255, 255, 255)
+RED = pygame.Color(255, 0, 0)
 
 
 def print_hi(name):
@@ -13,7 +15,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def main(winstyle=0):
+def main():
     if pygame.get_sdl_version()[0] == 2:
         pygame.mixer.pre_init(44100, 32, 2, 1024)
     pygame.init()
@@ -21,6 +23,16 @@ def main(winstyle=0):
     winstyle = 0
     bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+
+    surface = pygame.surface(SCREENRECT.size)
+    screen.blit(surface,)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
 
 
 # Press the green button in the gutter to run the script.
