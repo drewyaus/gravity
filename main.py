ab21da842b085d3aee292b0440334a713848eb45
Andrew Bono <andrew.bono@kindredgroup.com>
2021-09-11 09:17:30 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Skift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pygame
+
+SCREENRECT = pygame.Rect(0, 0, 335 * 3, 186 * 3)
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def main(winstyle=0):
+    if pygame.get_sdl_version()[0] == 2:
+        pygame.mixer.pre_init(44100, 32, 2, 1024)
+    pygame.init()
+
+    winstyle = 0
+    bestdepth = pygame.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pygame.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
