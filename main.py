feff55cfa1272eebf98fb14e6067969d2ee3a845
samyaus <sam.ad.bono@gmail.com>
2023-11-05 11:21:53 +0100

    it works

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ MARGIN_SIZE = 100
 
 BALL_SIZE = 10
 
-GRAVITY = 0.5
+GRAVITY = pygame.math.Vector2(0, 0.25)
 
 TOP_WALL_NORMAL = pygame.math.Vector2(0, 1)
 BOTTOM_WALL_NORMAL = pygame.math.Vector2(0, -1)
@@ -27,13 +27,13 @@ RIGHT_WALL_NORMAL = pygame.math.Vector2(-1, 0)
 
 WALL_THICKNESS = 50
 TOP_WALL_RECT = pygame.Rect((MARGIN_SIZE, MARGIN_SIZE - WALL_THICKNESS),
-                            (SCREEN.get_width() - 2 * MARGIN_SIZE, WALL_THICKNESS))
+                            (SCREEN.get_width() - 1.5 * MARGIN_SIZE, WALL_THICKNESS))
 BOTTOM_WALL_RECT = pygame.Rect((MARGIN_SIZE, SCREEN.get_height() - MARGIN_SIZE),
-                               (SCREEN.get_width() - 2 * MARGIN_SIZE, WALL_THICKNESS))
+                               (SCREEN.get_width() - 1.5 * MARGIN_SIZE, WALL_THICKNESS))
 LEFT_WALL_RECT = pygame.Rect((MARGIN_SIZE - WALL_THICKNESS, MARGIN_SIZE),
-                             (WALL_THICKNESS, SCREEN.get_height() - 2 * MARGIN_SIZE))
+                             (WALL_THICKNESS, SCREEN.get_height() - 1.5 * MARGIN_SIZE))
 RIGHT_WALL_RECT = pygame.Rect((SCREEN.get_width() - MARGIN_SIZE, MARGIN_SIZE),
-                              (WALL_THICKNESS, SCREEN.get_height() - 2 * MARGIN_SIZE))
+                              (WALL_THICKNESS, SCREEN.get_height() - 1.5 * MARGIN_SIZE))
 
 balls = []
 walls = []
@@ -55,8 +55,6 @@ class Ball:
     def __init__(self, x, y, velocity):
         self.velocity = velocity
         self.pos = pygame.math.Vector2(x, y)
-        energy = GRAVITY * (BOTTOM_WALL_RECT.top - self.pos.y)
-        self.y_max = BOTTOM_WALL_RECT.top - energy / GRAVITY
         self.rect = pygame.Rect(self.pos, (0, 0)).inflate((BALL_SIZE, BALL_SIZE))
         pygame.draw.rect(SCREEN, pygame.Color(255, 0, 255), self.rect)
         pygame.draw.circle(SCREEN, WHITE, self.pos, BALL_SIZE / 2)
@@ -64,8 +62,7 @@ class Ball:
     def update(self):
         old_pos = pygame.math.Vector2(self.pos)
         self.pos = self.pos + self.velocity
-        y_velocity = math.sqrt(2 * GRAVITY * (self.pos.y - self.y_max)) + 0.001
-        self.velocity = pygame.math.Vector2(self.velocity.x, math.copysign(y_velocity, self.velocity.y))
+        self.velocity += GRAVITY
         self.rect = pygame.Rect(self.pos, (0, 0)).inflate((BALL_SIZE, BALL_SIZE))
         pygame.draw.rect(SCREEN, pygame.Color(255, 0, 255), self.rect)
         pygame.draw.circle(SCREEN, BLACK, old_pos, BALL_SIZE / 2)
@@ -91,10 +88,10 @@ def main():
     # right wall
     walls.append(Wall(RIGHT_WALL_RECT, RIGHT_WALL_NORMAL, pygame.Color(255, 0, 255)))
 
-    for i in range(0, 3):
+    for i in range(0, 100):
         rand_x = random.randint(LEFT_WALL_RECT.right, RIGHT_WALL_RECT.left)
         rand_y = random.randint(TOP_WALL_RECT.bottom, BOTTOM_WALL_RECT.top)
-        velocity = pygame.math.Vector2(random.randint(0, 9), 0)
+        velocity = pygame.math.Vector2(random.randint(0, 9), 0).rotate(random.randint(-10, 10))
         balls.append(Ball(rand_x, rand_y, velocity))
 
     while True:
@@ -109,13 +106,13 @@ def main():
                     ball.velocity = ball.velocity.reflect(wall.normal)
                     ball.update()
                     if wall.rect.colliderect(ball.rect):
-                        ball.pos = ball.pos + BALL_SIZE * wall.normal
+                            ball.pos = ball.pos + BALL_SIZE * wall.normal
         for wall in walls:
             wall.redraw()
 
         pygame.display.update()
 
-        CLOCK.tick(5)
+        CLOCK.tick(100001.5)
 
 
 # Press the green button in the gutter to run the script.
